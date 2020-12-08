69af5d3bced194ca8ddf73f9370d0def0a2b853c
Cnidarias <roessner.pascal@gmail.com>
2020-12-08 20:43:04 +0100

    Adds day 6

---
+++ day6/p1/main.py
@@ -0,0 +1,16 @@
+def main():
+    with open("../input.txt", "r") as f:
+        content = f.read()
+        groups = [g.split() for g in content.split("\n\n")]
+        answers = []
+        for group in groups:
+            yes_questions = set(list(group[0]))
+            for idx in range(1, len(group), 1):
+                yes_questions = yes_questions.intersection(set(list(group[idx])))
+            answers.append(len(yes_questions))
+        print(sum(answers))
+
+
+
+if __name__ == "__main__":
+    main()
